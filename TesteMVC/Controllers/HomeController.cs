efdfa13cf6a2f61105aef0e4eb3412745a4c59ff
ecolinx <ecolinx@outlook.com>
2015-09-11 17:41:18 -0300

    Check-in inicial do módulo TesteMVC

---
+++ TesteMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace TesteMVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index ()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName ();
+			var isMono = Type.GetType ("Mono.Runtime") != null;
+
+			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View ();
+		}
+
+		public JsonResult Lista()
+		{
+			var lista = new List<int> ();
+
+			for (int i = 0; i < 50; i++) {
+				lista.Add (i);
+			}
+
+			var tes = lista.TakeWhile(a => a < 15).LastOrDefault();
+
+			return Json(tes, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
---
+++ TesteMVC/Controllers/TesteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+
+namespace TesteMVC.Controllers
+{
+	public class TesteController : ApiController
+    {
+		public HttpResponseMessage Get()
+        {
+			var response = new HttpResponseMessage ();
+			var lista = new List<int> ();
+
+			for (int i = 0; i < 50; i++) {
+				lista.Add (i);
+			}
+
+			response = Request.CreateResponse (HttpStatusCode.OK, lista);
+
+			return response;
+        }
+    }
+}
